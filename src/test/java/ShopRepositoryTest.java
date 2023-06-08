a083a956e88c951a3a8dabe8e71fb7aa8a359ba8
Dora Charm <kovina.dora@gmail.com>
2023-06-08 16:52:51 +0500

    Started Task 1

---
+++ src/test/java/ShopRepositoryTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+
+    ShopRepository repo = new ShopRepository();
+
+    Product product1 = new Product(1, "Хлеб", 20);
+
+}
